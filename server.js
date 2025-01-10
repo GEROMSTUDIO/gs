eb836866ccdd6f390bc16e23412085556b42e89b
Glitch (glitch-hello-sqlite) <none>
2025-01-10 19:21:10 +0000

    🍍🍿 Checkpoint ./server.js:71087736/3

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const auth = require("/sqlite.js"); // Notre module modifié
+const auth = require("./sqlite.js"); // Notre module modifié
 const bodyParser = require("body-parser");
 
 const app = express();
