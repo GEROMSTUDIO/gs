0310ee03aa247a01e08f6a04a544d3a20f157f46
Glitch (geromstudio) <none>
2025-01-18 11:00:48 +0000

    🖖🍵 Checkpoint ./public/login.js:71087736/30213 ./public/index.html:71087736/64

--- public/login.js
+++ public/login.js
@@ -81,7 +81,11 @@ document.addEventListener("DOMContentLoaded", () => {
         const redirectURL = `/index.html?connect=true&uniqueId=${encodeURIComponent(
           result.user.unique_id
         )}`;
-        document.cookie = `uniqueId=${result.user.unique_id}; path=/; max-age=86400; secure; samesite=strict`;
+        const date = new Date();
+        date.setTime(date.getTime() + 86400 * 1000); // 24 heures en millisecondes
+        document.cookie = `uniqueId=${
+          result.user.unique_id
+        }; path=/; expires=${date.toUTCString()}; max-age=86400; secure; samesite=strict`;
         window.location.href = redirectURL;
       } else {
         // Afficher un message d'erreur si les identifiants sont incorrects
