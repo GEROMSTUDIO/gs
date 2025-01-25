91b324efe46f6df852370cd13e28963fa7ff3b3f
Glitch (geromstudio) <none>
2025-01-25 11:07:53 +0000

    Rewound to commit 457b9aafad165a55a8ecb27b24f7dfeff92e14c8 + 0 edits

--- server.mjs
+++ server.mjs
@@ -27,6 +27,7 @@ const upload = multer({
 
 // Ensuite vos autres middlewares
 app.use(bodyParser.json());
+app.use('/views', express.static(path.join(__dirname, 'views')));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
@@ -171,40 +172,23 @@ app.get("/profile-picture/:uniqueId", async (req, res) => {
   }
 });
 
-app.get("/private-page", async (req, res) => {
-  try {
-    const { uniqueId } = req.query;  // Récupérer le uniqueId depuis les paramètres de l'URL
-
-    if (!uniqueId) {
-      return res.status(403).json({ error: "Accès interdit : identifiant unique manquant" });
-    }
-
-    const result = await auth.verifyAccess(uniqueId);  // Vérifier l'accès en fonction du uniqueId
-    if (result.success && result.user.access === 1) {
-      res.status(200).send("<h1>Bienvenue sur la page privée</h1>");
-    } else {
-      res.status(403).json({ error: "Accès interdit : droits insuffisants" });
-    }
-  } catch (error) {
-    console.error("Erreur lors de la vérification d'accès:", error);
-    res.status(500).json({ error: "Erreur serveur" });
-  }
-});
-
 app.get("/check-access", async (req, res) => {
   try {
-    const { uniqueId } = req.query;  // Récupérer le uniqueId depuis les paramètres de l'URL
-
+    const { uniqueId } = req.query;
     if (!uniqueId) {
       return res.status(400).json({ error: "Identifiant unique manquant" });
     }
-
-    // Déléguer la vérification à la base de données
     const result = await auth.verifyAccess(uniqueId);
-
     if (result.success && result.access === 1) {
-      // Si l'accès est autorisé, envoyer le contenu de hidden.html
-      res.sendFile(path.join(__dirname, "views", "hidden.html"));
+      // Lire le contenu du fichier HTML
+      const htmlContent = fs.readFileSync(path.join(__dirname, "views", "hidden.html"), 'utf8');
+      
+      // Modifier le HTML pour inclure le script de manière sécurisée
+      const modifiedHtml = htmlContent.replace('</body>', `
+        <script src="/views/film.js"></script>
+      </body>`);
+      
+      res.send(modifiedHtml);
     } else {
       res.status(403).json({ error: "Accès interdit : droits insuffisants" });
     }
--- views/film.js
+++ views/film.js
@@ -32,6 +32,7 @@ function getUniqueIdFromCookie() {
 
   // Vérifie si l'URL actuelle ne contient pas déjà les paramètres
   if (!currentUrl.includes("connect=true") && uniqueId) {
+
   } else if (!uniqueId) {
     console.log("Non connecté");
   }
